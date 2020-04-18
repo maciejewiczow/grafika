ec75434337eb11392acdb8fd72447045368670b8
Mafciejewicz <melonik0.9846@gmail.com>
2020-04-19 00:29:52 +0200

    Fixed member name spelling

--- basic_shadery/CameraControls.h
+++ basic_shadery/CameraControls.h
@@ -1,4 +1,4 @@
-﻿#pragma once
+#pragma once
 
 #include "Camera.h"
 
@@ -9,14 +9,14 @@ namespace gl
 
     class CameraControls {
     public:
-        CameraControls(): m_view_unif(nullptr), m_perojection_unif(nullptr) {}
+        CameraControls(): m_view_unif(nullptr), m_projection_unif(nullptr) {}
 
         virtual void onUpdate() = 0;
         void setViewUniform(gl::Uniform<glm::mat4>& unif) { m_view_unif = &unif; }
-        void setProjectionUniform(gl::Uniform<glm::mat4>& unif) { m_perojection_unif = &unif; }
+        void setProjectionUniform(gl::Uniform<glm::mat4>& unif) { m_projection_unif = &unif; }
 
     protected:
         gl::Uniform<glm::mat4>* m_view_unif;
-        gl::Uniform<glm::mat4>* m_perojection_unif;
+        gl::Uniform<glm::mat4>* m_projection_unif;
     };
 }
